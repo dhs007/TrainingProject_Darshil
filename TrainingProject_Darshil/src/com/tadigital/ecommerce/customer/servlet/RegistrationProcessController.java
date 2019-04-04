c293f3dfc96a21865c09092093ee1bc1cb72a877
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2019-04-04 20:00:49 +0530

    Formatted the registartion file

--- TrainingProject_Darshil/src/com/tadigital/ecommerce/customer/servlet/RegistrationProcessController.java
+++ TrainingProject_Darshil/src/com/tadigital/ecommerce/customer/servlet/RegistrationProcessController.java
@@ -10,58 +10,67 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.sun.glass.ui.Window;
 import com.tadigital.ecommerce.customer.entity.Customer;
 import com.tadigital.ecommerce.customer.service.*;
 
 @WebServlet("/RegistrationProcessController")
 public class RegistrationProcessController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+
 		String name = request.getParameter("f1");
-		String email=request.getParameter("f2");
-		String password=request.getParameter("f3");
+		String email = request.getParameter("f2");
+		String password = request.getParameter("f3");
 
 		Customer customer = new Customer();
-		String firstname=null, lastname=null;
-		
-		    if(name.split("\\w+").length>1){
+		String firstname = null, lastname = null;
 
-		       lastname = name.substring(name.lastIndexOf(" ")+1);
-		       firstname = name.substring(0, name.lastIndexOf(' '));
-		    }
-		     else{
-		       firstname = name;
-		    }	
+		if (name.split("\\w+").length > 1) {
+
+			lastname = name.substring(name.lastIndexOf(" ") + 1);
+			firstname = name.substring(0, name.lastIndexOf(' '));
+		} else {
+			firstname = name;
+		}
 		customer.setFirstname(firstname);
 		customer.setLastname(lastname);
 		customer.setEmail(email);
 		customer.setPassword(password);
+		
 		CustomerService customerService = new CustomerService();
+		
 		boolean status = customerService.registerCustomer(customer);
-		if(status) {
+		if (status) {
 			HttpSession session = request.getSession();
 			session.setAttribute("CUSTOMERDATA", customer);
-			session.setAttribute("check", 1);
+			session.setAttribute("check", "reg_success");
+			
 			RequestDispatcher rd = request.getRequestDispatcher("SignInSignUpForms.jsp");
 			System.out.println("Registration Succesfull");
-			rd.forward(request, response);
+			try {
+				rd.forward(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} else {
 			HttpSession session = request.getSession();
-			session.setAttribute("check", 0);
-			RequestDispatcher rd = request.getRequestDispatcher("SignInSignUpForms.jsp");
+			session.setAttribute("check", "error_reg");
+			
 			System.out.println("Error");
-			//Write Error Message on the screen
-			rd.forward(request, response);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("SignInSignUpForms.jsp");
+			try {
+				rd.forward(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
-		
-		
+
 	}
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
-	{}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
 }
