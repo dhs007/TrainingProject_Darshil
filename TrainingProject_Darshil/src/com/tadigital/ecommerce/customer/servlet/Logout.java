9750d1c22e27ad90424e774e594b340d443f7eca
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2019-04-04 19:59:41 +0530

    Added the functionality of logout

--- TrainingProject_Darshil/src/com/tadigital/ecommerce/customer/servlet/Logout.java
+++ TrainingProject_Darshil/src/com/tadigital/ecommerce/customer/servlet/Logout.java
@@ -5,30 +5,40 @@ import java.io.IOException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class Logout
- */
 @WebServlet("/Logout")
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
- 
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		HttpSession session = request.getSession();
+		Cookie[] cookies = request.getCookies();
+		
+		for (int i = 0; i < cookies.length; i++) {
+			if (cookies[i].getName().equals("abc")) {
+				cookies[i].setMaxAge(0);
+				response.addCookie(cookies[i]);
+
+			}
+		}
+		
 		session.invalidate();
-		RequestDispatcher rd = request.getRequestDispatcher("SignInSignUpForms.jsp");
-		rd.forward(request, response);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
+		try {
+			rd.forward(request, response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 	}
 
 }
