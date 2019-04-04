c5210df1c8886ddc799a7044bbbc16532e9875ba
Darshil Shah <shahdarshil_sh@srmuniv.edu.in>
2019-04-04 19:58:59 +0530

    Added stay signed in functionality

--- TrainingProject_Darshil/src/com/tadigital/ecommerce/customer/servlet/LoginProcessController.java
+++ TrainingProject_Darshil/src/com/tadigital/ecommerce/customer/servlet/LoginProcessController.java
@@ -3,6 +3,7 @@ package com.tadigital.ecommerce.customer.servlet;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,40 +12,109 @@ import javax.servlet.RequestDispatcher;
 
 import com.tadigital.ecommerce.customer.entity.Customer;
 import com.tadigital.ecommerce.customer.service.*;
-/**
- * Servlet implementation class LoginProcessController
- */
+
 @WebServlet("/login")
 public class LoginProcessController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		String email=request.getParameter("f1");
-		String password=request.getParameter("f2");
 		Customer customer = new Customer();
-		customer.setEmail(email);
-		customer.setPassword(password);
 		CustomerService customerService = new CustomerService();
-		boolean status = customerService.loginCustomer(customer);
-		if(status) {
-			HttpSession session = request.getSession();
-			session.setAttribute("CUSTOMERDATA", customer);
-			session.setAttribute("check", 2);
-			RequestDispatcher rd = request.getRequestDispatcher("CustomerAccounts.jsp");
-			rd.forward(request, response);
-		} else {
-			HttpSession session = request.getSession();
-			session.setAttribute("check", -1);
-			RequestDispatcher rd = request.getRequestDispatcher("SignInSignUpForms.jsp");
-			//Write Error Message on the screen
-			rd.forward(request, response);
-		}
+		HttpSession session = request.getSession();
 		
+		String cValue = (String) session.getAttribute("COOKIEVALUE");
+		String email = null;
+		String password = null;
+		long lastLogInTime;
+		
+		if (cValue == null) {
+			email = request.getParameter("f1");
+			password = request.getParameter("f2");
+
+			customer.setEmail(email);
+			customer.setPassword(password);
+
+			boolean status = customerService.loginCustomer(customer);
+			if (status) {
+
+				session.setAttribute("CUSTOMERDATA", customer);
+				String staySignedIn = request.getParameter("persist");
+				if (staySignedIn != null) {
+					long curLogInTime = System.currentTimeMillis();
+					customer.setLastLogInTime(curLogInTime);
+					cValue = email + "-" + curLogInTime;
+					
+					Cookie cookie = new Cookie("abc", cValue);
+					cookie.setMaxAge(60 * 60 * 24 * 30);
+					
+					response.addCookie(cookie);
+					
+					boolean s = customerService.updateAfterCookie(customer);
+					if (s) {
+						System.out.println("Cookie Added");
+					} else {
+						System.out.println("Error Adding Cookie");
+					}
+
+				}
+				RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
+				try {
+					rd.forward(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			} else {
+				session.setAttribute("check", "not_login");
+				RequestDispatcher rd = request.getRequestDispatcher("SignInSignUpForms.jsp");
+				try {
+					rd.forward(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
 	}
+
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Customer customer = new Customer();
+		CustomerService customerService = new CustomerService();
+		HttpSession session = request.getSession();
+		
+		String cValue = (String) session.getAttribute("COOKIEVALUE");
+		String email = null, password = null;
+
+		String[] data = cValue.split("-");
+		email = data[0];
+		
+		long lastLogInTime;
+		lastLogInTime = Long.parseLong(data[1]);
+		customer.setEmail(email);
+		customer.setLastLogInTime(lastLogInTime);
+
+		boolean status = customerService.loginCustomerCookie(customer);
+		
+		if (status) {
+
+			session.setAttribute("CUSTOMERDATA", customer);
+
+			RequestDispatcher rd = request.getRequestDispatcher("CustomerAccount.jsp");
+			try {
+				rd.forward(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("SignInSignUpForms.jsp");
+			try {
+				rd.forward(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
